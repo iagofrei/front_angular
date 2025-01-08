6ec33516c62f3bb0738424fe9d4cb783a987a4ac
Iago <iago.g.freitas@gmail.com>
2025-01-08 16:10:21 -0300

    nova aula

---
+++ sessao_05_projeto_filtro_de_lista_de_usuarios/users-filter-project/src/app/components/components.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { AngularMaterialModule } from "../angular-material/angular-maretial.module";
+
+@NgModule({
+    declarations: [],
+    imports: [
+        AngularMaterialModule
+    ],
+    exports: []
+})
+
+export class ComponentsModule { }
