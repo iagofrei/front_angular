0536a73343d7f371818da9f94739a526b99d65df
Iago <iago.g.freitas@gmail.com>
2025-01-09 22:54:12 -0300

    criando o evento de clique no usuario

--- sessao_05_projeto_filtro_de_lista_de_usuarios/users-filter-project/src/app/components/users-list/users-list.component.ts
+++ sessao_05_projeto_filtro_de_lista_de_usuarios/users-filter-project/src/app/components/users-list/users-list.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { IUser } from '../../interfaces/user/user.interface';
 import { UsersList } from '../../data/users-list';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.scss'
+  styleUrl: './users-list.component.scss',
 })
 export class UsersListComponent {
   displayedColumns: string[] = ['name', 'date', 'status'];
   usersList: IUser[] = UsersList;
 
+  @Output('userSelected') userSelectedEmitt = new EventEmitter<IUser>();
+
   onUserSelected(user: IUser) {
-    console.log('user', user)
+    console.log('user', user);
+
+    this.userSelectedEmitt.emit(user);
   }
 }
--- sessao_05_projeto_filtro_de_lista_de_usuarios/users-filter-project/src/app/pipes/phone.pipe.ts
+++ sessao_05_projeto_filtro_de_lista_de_usuarios/users-filter-project/src/app/pipes/phone.pipe.ts
@@ -6,12 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PhonePipe implements PipeTransform {
   transform(phone: string): string {
     const IVALID_PHONE = !phone || phone.length < 10 || phone.length > 11;
-    const CELLPHONE = phone.length == 11;
-
+    
     if (IVALID_PHONE) {
       return 'Telefone indisponível ou invalido';
     }
-
+    
+    const CELLPHONE = phone.length == 11;
+    
     if (CELLPHONE) {
       return `(${phone.substring(0, 2)}) ${phone.substring(2, 7)}-${phone.substring(7)}`;
     } else {
